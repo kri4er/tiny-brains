5de8c56af06030eba28c7bb57a33aa329429e223
kri4er <mynamekaldery@gmail.com>
2024-02-21 00:38:16 +0100

    fix: Configure styling to use server generated style file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     darkMode: ['class', '[data-mode="dark"]'],
-    content: ["./src/**/*.{html,js}"],
+    content: ["./templates/**/*.{html,js}", './templates/*{html,js}'],
     theme: {
         extend: {},
     },
